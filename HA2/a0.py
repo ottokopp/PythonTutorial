f007507b736bca760f1e42b92a9ad87da9ea4096
ottokopp <kopp_otto@yahoo.de>
2026-02-03 22:10:28 +0100

    a0

---
+++ HA2/a0.py
@@ -0,0 +1,15 @@
+"""
+Keine richtige Aufgabe, aber überleg dir mal, was dich in deinem Alltag nervt, irgendein Kleinscheiß bei der Arbeit der irgendwie dumm ist
+oder irgendwas an deinem PC oder deinem Handy, was du echt praktisch fändest.
+Vollkommen ungeachtet dessen, wie umsetzbat du das findest.
+In der Regel hat niemand, inklusive erfahrenen Programmierern, irgeindeinen Plan davon, das einfach und was schwer umzusetzen ist :)
+Das ist jetzt Fantasy-Christmas-Land. Fantasier mal was zusammen.
+"""
+
+print("Mich nervt bzw das fände ich richtig praktisch:" \
+        "" \
+        "" \
+        "" \
+        "" \
+        "" \
+    )
